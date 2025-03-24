d1695dfdbdd4ad4d889c8409a3c8f7dd106a5590
And75 <andreaporcella@gmail.com>
2025-03-24 17:56:36 +0100

    Update forge.config.ts

--- forge.config.ts
+++ forge.config.ts
@@ -1,14 +1,12 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
-// import { MakerSquirrel } from '@electron-forge/maker-squirrel';
-// import { MakerZIP } from '@electron-forge/maker-zip';
-// import { MakerDeb } from '@electron-forge/maker-deb';
-// import { MakerRpm } from '@electron-forge/maker-rpm';
+import { MakerSquirrel } from '@electron-forge/maker-squirrel';
+import { MakerZIP } from '@electron-forge/maker-zip';
+import { MakerDeb } from '@electron-forge/maker-deb';
+import { MakerRpm } from '@electron-forge/maker-rpm';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 import path from 'path';
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
-import {platform, arch } from 'os'; // Import the 'arch' property from the 'os' module
-import packageJSON from './package.json';
 
 const config: ForgeConfig = {
   packagerConfig: {
@@ -22,17 +20,10 @@ const config: ForgeConfig = {
   },
   rebuildConfig: {},
   makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {
-        //name: "sairen_ai_audio_solution",
-        setupExe: `${packageJSON.productName}-${packageJSON.version}-${platform}-${arch}.exe`,
-        // The ICO file to use as the icon for the generated Setup.exe
-        setupIcon: './icon/icon.ico',
-        //loadingGif: './icon/installer.gif',
-      }
-    }
-  ],
+    new MakerSquirrel({}), 
+    new MakerZIP({}, ['darwin']), 
+    new MakerRpm({}), 
+    new MakerDeb({})],
   plugins: [
     new WebpackPlugin({
       mainConfig,
